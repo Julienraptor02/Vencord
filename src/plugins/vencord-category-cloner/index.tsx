04a720ae6c2a82092667a277b6a39afc6abbfec4
Julienraptor02 <julienraptor02@gmail.com>
2025-03-02 18:14:16 +0000

    fix vencord-category-cloner

--- src/plugins/vencord-category-cloner/index.tsx
+++ src/plugins/vencord-category-cloner/index.tsx
@@ -69,7 +69,7 @@ function MakeContextCallback(name: "Channel"): NavContextMenuPatchCallback {
 export default definePlugin({
     name: "Clone Category",
     description: "This plugin adds ability to clone category channels.",
-    authors: [{ name: "mafineeek", id: 854342480019587133n }],
+    authors: [Devs.Mafineek],
     contextMenus: {
         "channel-context": MakeContextCallback("Channel")
     }
