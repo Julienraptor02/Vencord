ffd34e4b09d8986ffe3005db499db7cbf2a99392
Julienraptor02 <julienraptor02@gmail.com>
2025-04-05 22:11:59 +0000

    Revert "Fix size of chat bar buttons on new ui (#3351)"
    This reverts commit 094a7852b1ebc0f5e2e526e942bf93f1a5397f9c.

--- src/plugins/invisibleChat.desktop/index.tsx
+++ src/plugins/invisibleChat.desktop/index.tsx
@@ -79,8 +79,8 @@ const ChatBarIcon: ChatBarButtonFactory = ({ isMainChat }) => {
             <svg
                 aria-hidden
                 role="img"
-                width="20"
-                height="20"
+                width="24"
+                height="24"
                 viewBox={"0 0 64 64"}
                 style={{ scale: "1.39", translate: "0 -1px" }}
             >
