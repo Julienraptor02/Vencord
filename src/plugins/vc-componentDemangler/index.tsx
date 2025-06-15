40c4666f7d5dee3b06bebe33096b76488d3ea3a1
Julienraptor02 <julienraptor02@gmail.com>
2025-06-15 16:57:25 +0000

    fix vc-componentDemangler not having a description

--- src/plugins/vc-componentDemangler/index.tsx
+++ src/plugins/vc-componentDemangler/index.tsx
@@ -209,7 +209,7 @@ const iconFilter = filters.componentByCode(".colors.INTERACTIVE_NORMAL,colorClas
 export default definePlugin({
     // Avoid conflict with https://github.com/Vendicated/Vencord/pull/3257
     name: "ComponentDemangler_",
-    description: "",
+    description: "Make React DevTools nicer to use",
     authors: [Devs.sadan],
     startAt: StartAt.Init,
     patches: [
