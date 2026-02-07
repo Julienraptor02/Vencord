860aef9b3f8b4fd0fc4db7e237a3e8145b174964
Julienraptor02 <julienraptor02@gmail.com>
2026-02-07 03:23:05 +0000

    fix vc-NewlinesInCommands

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -633,6 +633,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "prism",
         id: 390884143749136386n,
     },
+    Suffocate: {
+        name: "Suffocate",
+        id: 772601756776923187n,
+    },
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
