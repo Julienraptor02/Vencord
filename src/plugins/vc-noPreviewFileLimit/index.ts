80af4e7d90501cab64511d72c50873820c8717c3
Julienraptor02 <julienraptor02@gmail.com>
2026-05-01 10:50:51 +0000

    add noPreviewFileLimit

---
+++ src/plugins/vc-noPreviewFileLimit/index.ts
@@ -0,0 +1,23 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2025 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { Devs } from "@utils/constants";
+import definePlugin from "@utils/types";
+
+export default definePlugin({
+    name: "NoPreviewFileLimit",
+    description: "Allows you to fully preview files in chat",
+    authors: [Devs.Julienraptor01],
+    patches: [
+        {
+            find: 'Range:"bytes=0-50000",',
+            replacement: {
+                match: /Range:"bytes=0-50000",/,
+                replace: "",
+            },
+        }
+    ],
+});
